1ce6ac494ceb773bcdcdd7d87a468c9a499058a4
Marta Smuk <msmuk999@gmail.com>
2024-05-07 01:27:05 +0200

    average calculation

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let nFirstNumber = +prompt("Please enter first number");
+let nSecondNumber = +prompt("Please enter first number");
+let nThirdNumber = +prompt("Please enter first number");
+
+let nAverage = (nFirstNumber + nSecondNumber + nThirdNumber) / 3;
+alert("Average number is: "+ nAverage);
